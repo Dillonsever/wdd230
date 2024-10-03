96bba989cdd180fcf7e0d063079505f1f9c67507
dillonsever <dillonsever@gmail.com>
2024-10-03 13:09:54 -0600

    03 Assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,16 @@ yearElement.textContent = currentYear;
 
 const lastModifiedElement = document.getElementById("lastModified");
 lastModifiedElement.textContent = `Last Updated: ${document.lastModified}`;
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+      menu.classList.add('visible');
+      this.innerHTML = '✖️'; // Change to X when open
+    } else {
+      menu.classList.remove('visible');
+      menu.classList.add('hidden');
+      this.innerHTML = '&#9776;'; // Change back to hamburger icon
+    }
+  });
